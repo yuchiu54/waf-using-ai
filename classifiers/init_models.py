99fb7d0f878f08c091dc255c3a285fcc6e2bb1d1
yuchiu54 <ycwaw15@gmail.com>
2024-02-02 10:41:31 +0800

    refactor init_models.py

--- classifiers/init_models.py
+++ classifiers/init_models.py
@@ -8,17 +8,11 @@ from rf import init_rf
 
 def setup_pretrain_model(names, init_model_functions):
     df = pd.read_csv("../data/train_data.csv")
-    for item in tuple(zip(names, init_model_functions)):
-        print(item[1])
-        model = item[1](df)
-        dump(model, f"live/{item[0]}.joblib")
-
-#        with open(f"../classifiers/live/{item[0]}.pkl", "wb") as file:
-#            pickle.dump(model, file)
-#        file.close()
-
-#        dump(item[1], f"shadow/{item[0]}.joblib")
-    print("save models to live and shadow folder")
+    for name, func in zip(names, init_model_functions):
+        model = func(df)
+        dump(model, f"live/{name}.joblib")
+#        dump(item[1], f"shadow/{name}.joblib")
+    print("save models to live and folder")
 
 if __name__ == "__main__":
     names = ["predictor_svm", "predictor_rf"]
